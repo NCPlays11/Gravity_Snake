74ceea1cf0670635f7984641304a2f9ffba92776
NCPlays11 <nc11plays@gmail.com>
2026-01-27 15:37:11 -0600

    Using conf.lua

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+    t.window.title = "Gravity Snake"
+    t.window.fullscreen = true
+end
--- main.lua
+++ main.lua
@@ -1,13 +1,11 @@
 function love.load()
-    tileManager = require "tileManager"
-
-    tileManager:load()
+    
 end
 
 function love.update(dt)
-    tileManager:update(dt)
+    
 end
 
 function love.draw()
-    tileManager:draw()
+    
 end
