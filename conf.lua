7bd7a13ac7e102c4e5b3198381b5367dcefe5d63
NCPlays11 <nc11plays@gmail.com>
2026-01-28 15:10:48 -0600

    Added movement system + updated vsync value

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
     t.window.title = "Gravity Snake"
     t.window.fullscreen = true
-    t.window.vsync = false
+    t.window.vsync = 0
+    t.window.display = 1
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,26 @@
 function love.load()
-    
+    snake = {}
+    snake.x = 200
+    snake.y = 250
 end
 
 function love.update(dt)
-    
+
 end
 
 function love.draw()
-    
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle("fill", snake.x, snake.y, 50, 50)
+end
+
+function love.keyreleased(key)
+   if key == "w" then
+      snake.y = snake.y - 50
+   elseif key == "s" then
+      snake.y = snake.y + 50
+   elseif key == "a" then
+      snake.x = snake.x - 50
+   elseif key == "d" then
+      snake.x = snake.x + 50
+   end
 end
